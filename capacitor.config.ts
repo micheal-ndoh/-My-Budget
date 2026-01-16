3791efcc64fc1e7a9eda6dcbea36e36004db66e2
micheal-ndoh <michaelndoh9@gmail.com>
2026-01-16 09:53:19 +0100

    Setup Codemagic iOS build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.vercel.my_budget_amber',
+  appName: 'My Budget',
+  webDir: 'dist'
+};
+
+export default config;
